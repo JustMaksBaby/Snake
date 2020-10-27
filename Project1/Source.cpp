be29c2a47c1a086614cf99357eb8dea97409c0c4
Just Maks <just.maks.bro@gmail.com>
2020-10-27 23:18:04 +0200

    Set color for food

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -3,17 +3,18 @@
 
 
 #pragma comment (lib, "User32.lib")
+#pragma comment (lib, "Kernel32.lib")
 
 //in characters
-static const int g_ScreenWidth  = 100; 
-static const int g_ScreenHeight = 60;  
+static const int g_ScreenWidth  = 80; 
+static const int g_ScreenHeight = 30;  
 
 
 struct SnakeSegment
 {
 	//position in chars 
-	int x; 
-	int y; 
+	short x; 
+	short y; 
 };
 
 
@@ -25,23 +26,20 @@ void  InitTopBar(wchar_t * buffer)
 		buffer[2 * g_ScreenWidth + i] = L'=';
 	}
 
-	wsprintfW(buffer + g_ScreenWidth + 55, L"S.N.A.K.E"); 
+	wsprintfW(buffer + g_ScreenWidth + 30, L"S.N.A.K.E"); 
 }
 
 int main()
 {
 	//create console buffer
 	constexpr int BUFFER_SIZE = g_ScreenHeight * g_ScreenWidth;
-	wchar_t* screenBuffer = new wchar_t[BUFFER_SIZE];
+	wchar_t* screenBuffer = new wchar_t[BUFFER_SIZE] {};
 	//init  map
-	//for (int i = 0; i < BUFFER_SIZE; ++i)
-	//{
-	//	screenBuffer[i] = L' '; 
-	//}
 
 
 	//set console buffer 
 	HANDLE  hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL); 
+	SetConsoleScreenBufferSize(hConsole, {g_ScreenWidth, g_ScreenHeight});
 	SetConsoleActiveScreenBuffer(hConsole); 
 
 	//count chars written in console
@@ -53,22 +51,21 @@ int main()
 	int  score = 0; 
 
 
-	//snake budy
+	//snake body
 	std::list<SnakeSegment> snake = { {60,15}, {61,15}, {62,15} };  
 
 
 	//food position
 	// on map can be only one food element 
-	int foodX = 15; 
-	int foodY = 13; 
+	short foodX = 15; 
+	short foodY = 13; 
 
 
 	InitTopBar(screenBuffer);  
-
 	//game loop 
 	while (1)
 	{
-		//draw snake budy
+		//draw snake body
 		for (auto segment : snake)
 		{
 			screenBuffer[g_ScreenWidth * segment.y + segment.x] = isDead ? L'+' : L'0'; 
@@ -79,9 +76,12 @@ int main()
 
 		//draw a food 
 		screenBuffer[g_ScreenWidth * foodY + foodX] = L'$';
+		//change color for food
+		DWORD  coloredChars = 0;
+		FillConsoleOutputAttribute(hConsole, 10, 1, { foodX,foodY }, &coloredChars);
 
 
-
+		int a = 2.3f; 
 		//Display frame
 		WriteConsoleOutputCharacterW(hConsole, screenBuffer, g_ScreenHeight * g_ScreenWidth, { 0, 0 }, &bytesWritten);
 	}
